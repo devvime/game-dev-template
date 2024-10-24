7bfd058ba7d4ec624033da7bb5b13ea24a9cf530
Victor <vmendes@engebras.com.br>
2024-10-24 18:08:03 -0300

    test physics

--- Tests/test1.py
+++ Tests/test1.py
@@ -3,69 +3,100 @@ import pybullet as p
 import pybullet_data
 import math
 
-class Player(Entity):
-    def __init__(self, physics_client):
-        super().__init__(
-            model='cube',
-            color=color.orange,
-            position=(0, 2, 0)  # Inicialmente acima do chão
-        )
-        self.speed = 5
-        self.jump_height = 5
-        self.physics_client = physics_client
+# Definir a variável global para o cliente de física
+physics_client = None
 
-        # Criação do corpo rígido (rigid body) no PyBullet
-        self.player_collider = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5], physicsClientId=self.physics_client)
-        self.player_body = p.createMultiBody(
-            baseMass=1,  # Massa do corpo, define se será afetado por física
-            baseCollisionShapeIndex=self.player_collider,
-            basePosition=self.position,
-            physicsClientId=self.physics_client
+class PhysicsEntity(Entity):
+    def __init__(self, model='cube', position=(0, 0, 0), mass=1, collision_shape='box', **kwargs):
+        super().__init__(model=model, position=position, **kwargs)
+        
+        global physics_client
+        if physics_client is None:
+            physics_client = p.connect(p.DIRECT)
+            p.setGravity(0, -200, 0, physicsClientId=physics_client)
+            p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+        # Adicionar uma cápsula de colisão baseada na escala do objeto
+        if collision_shape == 'capsule':
+            radius = self.scale_x / 2  # Raio com base na largura do modelo
+            height = self.scale_y - 2 * radius  # Altura do corpo, excluindo as extremidades arredondadas
+            self.collision_shape = p.createCollisionShape(p.GEOM_CAPSULE, radius=radius, height=height, physicsClientId=physics_client)
+        else:
+            self.collision_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[self.scale_x / 2, self.scale_y / 2, self.scale_z / 2], physicsClientId=physics_client)
+
+        # Criar corpo rígido
+        self.body = p.createMultiBody(
+            baseMass=mass,
+            baseCollisionShapeIndex=self.collision_shape,
+            basePosition=position,
+            physicsClientId=physics_client
         )
-        p.changeDynamics(self.player_body, -1, lateralFriction=1)
 
     def update(self):
-        # Sincroniza o movimento e rotação do PyBullet com a Ursina
-        pos, orn = p.getBasePositionAndOrientation(self.player_body, physicsClientId=self.physics_client)
-        self.position = Vec3(pos)  # Atualiza a posição do player na Ursina
-
-        # Converter o quaternion para ângulos de Euler
+        # Sincroniza a posição e rotação com o PyBullet
+        pos, orn = p.getBasePositionAndOrientation(self.body, physicsClientId=physics_client)
+        self.position = Vec3(pos)
+        
         euler = p.getEulerFromQuaternion(orn)
-        self.rotation = Vec3(math.degrees(euler[0]), math.degrees(euler[1]), math.degrees(euler[2]))  # Atualiza a rotação do player
+        self.rotation = Vec3(0, math.degrees(euler[1]), 0)  # Somente rotação no eixo Y
 
-        # Movimentação básica com W, A, S, D
-        if held_keys['w']:
-            p.applyExternalForce(self.player_body, -1, (0, 0, -self.speed), (0, 0, 0), p.WORLD_FRAME, physicsClientId=self.physics_client)
-        if held_keys['s']:
-            p.applyExternalForce(self.player_body, -1, (0, 0, self.speed), (0, 0, 0), p.WORLD_FRAME, physicsClientId=self.physics_client)
+    def apply_force(self, force, position=None):
+        p.applyExternalForce(self.body, -1, force, position if position else (0, 0, 0), p.WORLD_FRAME, physicsClientId=physics_client)
+
+    def set_velocity(self, velocity):
+        p.resetBaseVelocity(self.body, linearVelocity=velocity, physicsClientId=physics_client)
+
+    def get_velocity(self):
+        vel, _ = p.getBaseVelocity(self.body, physicsClientId=physics_client)
+        return Vec3(vel)
+
+class Player(PhysicsEntity):
+    def __init__(self):
+        super().__init__(model='cube', position=(0, 2, 0), mass=1, color=color.orange, scale=(1, 2, 1), collision_shape='capsule')
+        self.speed = 15
+        self.jump_height = 15
+        self.grounded = False
+
+    def update(self):
+        super().update()
+
+        # Checar se o player está no chão
+        contact_points = p.getContactPoints(self.body, physicsClientId=physics_client)
+        self.grounded = len(contact_points) > 0
+
+        movement = Vec3(0, 0, 0)
+
+        # Mover o player lateralmente com as teclas 'A' e 'D'
         if held_keys['a']:
-            p.applyExternalForce(self.player_body, -1, (-self.speed, 0, 0), (0, 0, 0), p.WORLD_FRAME, physicsClientId=self.physics_client)
+            movement -= self.right * self.speed  # Mover para a esquerda
         if held_keys['d']:
-            p.applyExternalForce(self.player_body, -1, (self.speed, 0, 0), (0, 0, 0), p.WORLD_FRAME, physicsClientId=self.physics_client)
+            movement += self.right * self.speed  # Mover para a direita
+        
+        # Mover o player para frente e para trás com base na orientação
+        if held_keys['w']:
+            movement += self.forward * self.speed
+        if held_keys['s']:
+            movement -= self.forward * self.speed
+        
+        # Obter a velocidade atual para manter o movimento suave
+        current_velocity = self.get_velocity()
 
-        # Pular (aplica força para cima)
-        if held_keys['space']:
-            p.applyExternalForce(self.player_body, -1, (0, self.jump_height, 0), (0, 0, 0), p.WORLD_FRAME, physicsClientId=self.physics_client)
+        # Aplicar a nova velocidade
+        self.set_velocity((movement.x, current_velocity.y, movement.z))
+
+        # Saltar
+        if self.grounded and held_keys['space']:
+            self.set_velocity((movement.x, self.jump_height, movement.z))
 
 def update():
     p.stepSimulation(physics_client)
 
 app = Ursina(borderless=False)
 
-# Inicialização do mundo de física no PyBullet
-physics_client = p.connect(p.DIRECT)  # Alternativamente, use p.GUI para uma visualização
-p.setGravity(0, -9.81, 0, physicsClientId=physics_client)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Caminho para modelos padrão do PyBullet
+ground = PhysicsEntity(model='plane', scale=(10, 1, 10), position=(0, 0, 0), texture='grass', mass=0)
 
-# Criação do chão
-ground_shape = p.createCollisionShape(p.GEOM_PLANE, physicsClientId=physics_client)
-ground_body = p.createMultiBody(0, ground_shape, physicsClientId=physics_client)  # Massa 0 significa que é estático
+player = Player()
 
-# Criar o chão visual na Ursina
-ground = Entity(model='plane', scale=(10, 1, 10), color=color.green)
+EditorCamera()
 
-# Instanciar o player
-player = Player(physics_client)
-
-# Chamar a simulação do PyBullet a cada frame
 app.run()
--- Tests/test2.py
+++ Tests/test2.py
@@ -1,78 +1,111 @@
 from ursina import *
-import pybullet as p
-import pybullet_data
-import math
-
-# Definir a variável global para o cliente de física
-physics_client = None
-
-class PhysicsEntity(Entity):
-    def __init__(self, model='cube', position=(0, 0, 0), mass=1, **kwargs):
-        super().__init__(model=model, position=position, **kwargs)
-        
-        global physics_client  # Acessar a variável global
-        # Inicializar o cliente de física do PyBullet
-        if physics_client is None:
-            physics_client = p.connect(p.DIRECT)
-            p.setGravity(0, -100, 0, physicsClientId=physics_client)
-            p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        
-        # Criar a forma de colisão e o corpo rígido
-        self.collision_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.5, 0.5, 0.5], physicsClientId=physics_client)
-        self.body = p.createMultiBody(
-            baseMass=mass,
-            baseCollisionShapeIndex=self.collision_shape,
-            basePosition=position,
-            physicsClientId=physics_client
-        )
-
-    def update(self):
-        # Sincroniza a posição e rotação com o PyBullet
-        pos, orn = p.getBasePositionAndOrientation(self.body, physicsClientId=physics_client)
-        self.position = Vec3(pos)
-        
-        # Converter o quaternion para ângulos de Euler
-        euler = p.getEulerFromQuaternion(orn)
-        self.rotation = Vec3(math.degrees(euler[0]), math.degrees(euler[1]), math.degrees(euler[2]))
-
-    def apply_force(self, force, position=None):
-        p.applyExternalForce(self.body, -1, force, position if position else (0, 0, 0), p.WORLD_FRAME, physicsClientId=physics_client)
-
-class Player(PhysicsEntity):
-    def __init__(self):
-        super().__init__(model='cube', position=(0, 2, 0), mass=1, color=color.orange)
-        self.speed = 5
-        self.jump_height = 5
-
-    def update(self):
-        super().update()  # Chama a atualização da classe base
-        
-        # Movimentação básica com W, A, S, D
-        if held_keys['w']:
-            self.apply_force((0, 0, -self.speed))
-        if held_keys['s']:
-            self.apply_force((0, 0, self.speed))
-        if held_keys['a']:
-            self.apply_force((-self.speed, 0, 0))
-        if held_keys['d']:
-            self.apply_force((self.speed, 0, 0))
-        
-        # Pular
-        if held_keys['space']:
-            self.apply_force((0, self.jump_height, 0))
-
-def update():
-    p.stepSimulation(physics_client)  # Agora pode acessar a variável global
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.shaders import lit_with_shadows_shader
 
 app = Ursina(borderless=False)
 
-# Criar o chão visual na Ursina
-ground = PhysicsEntity(model='plane', scale=(10, 1, 10), position=(0, 0, 0), texture='grass', collider='box', mass=0)
+random.seed(0)
+Entity.default_shader = lit_with_shadows_shader
 
-# Instanciar o player
-player = Player()
+ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
 
-EditorCamera()
+editor_camera = EditorCamera(enabled=False, ignore_paused=True)
+player = FirstPersonController(model='cube', z=-10, color=color.orange, origin_y=-.5, speed=8, collider='box')
+player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
 
-# Chamar a simulação do PyBullet a cada frame
+gun = Entity(model='cube', parent=camera, position=(.5,-.25,.25), scale=(.3,.2,1), origin_z=-.5, color=color.red, on_cooldown=False)
+gun.muzzle_flash = Entity(parent=gun, z=1, world_scale=.5, model='quad', color=color.yellow, enabled=False)
+
+shootables_parent = Entity()
+mouse.traverse_target = shootables_parent
+
+
+for i in range(16):
+    Entity(model='cube', origin_y=-.5, scale=2, texture='brick', texture_scale=(1,2),
+        x=random.uniform(-8,8),
+        z=random.uniform(-8,8) + 8,
+        collider='box',
+        scale_y = random.uniform(2,3),
+        color=color.hsv(0, 0, random.uniform(.9, 1))
+        )
+
+def update():
+    if held_keys['left mouse']:
+        shoot()
+
+def shoot():
+    if not gun.on_cooldown:
+        # print('shoot')
+        gun.on_cooldown = True
+        gun.muzzle_flash.enabled=True
+        from ursina.prefabs.ursfx import ursfx
+        ursfx([(0.0, 0.0), (0.1, 0.9), (0.15, 0.75), (0.3, 0.14), (0.6, 0.0)], volume=0.5, wave='noise', pitch=random.uniform(-13,-12), pitch_change=-12, speed=3.0)
+        invoke(gun.muzzle_flash.disable, delay=.05)
+        invoke(setattr, gun, 'on_cooldown', False, delay=.15)
+        if mouse.hovered_entity and hasattr(mouse.hovered_entity, 'hp'):
+            mouse.hovered_entity.hp -= 10
+            mouse.hovered_entity.blink(color.red)
+
+
+from ursina.prefabs.health_bar import HealthBar
+
+class Enemy(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(parent=shootables_parent, model='cube', scale_y=2, origin_y=-.5, color=color.light_gray, collider='box', **kwargs)
+        self.health_bar = Entity(parent=self, y=1.2, model='cube', color=color.red, world_scale=(1.5,.1,.1))
+        self.max_hp = 100
+        self.hp = self.max_hp
+
+    def update(self):
+        dist = distance_xz(player.position, self.position)
+        if dist > 40:
+            return
+
+        self.health_bar.alpha = max(0, self.health_bar.alpha - time.dt)
+
+
+        self.look_at_2d(player.position, 'y')
+        hit_info = raycast(self.world_position + Vec3(0,1,0), self.forward, 30, ignore=(self,))
+        # print(hit_info.entity)
+        if hit_info.entity == player:
+            if dist > 2:
+                self.position += self.forward * time.dt * 5
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, value):
+        self._hp = value
+        if value <= 0:
+            destroy(self)
+            return
+
+        self.health_bar.world_scale_x = self.hp / self.max_hp * 1.5
+        self.health_bar.alpha = 1
+
+# Enemy()
+enemies = [Enemy(x=x*4) for x in range(4)]
+
+
+def pause_input(key):
+    if key == 'tab':    # press tab to toggle edit/play mode
+        editor_camera.enabled = not editor_camera.enabled
+
+        player.visible_self = editor_camera.enabled
+        player.cursor.enabled = not editor_camera.enabled
+        gun.enabled = not editor_camera.enabled
+        mouse.locked = not editor_camera.enabled
+        editor_camera.position = player.position
+
+        application.paused = editor_camera.enabled
+
+pause_handler = Entity(ignore_paused=True, input=pause_input)
+
+
+sun = DirectionalLight()
+sun.look_at(Vec3(1,-1,-1))
+Sky()
+
 app.run()
